b99450914cd204e75bcd5c3211be235f86eddd5f
Marcelo <marcelofscarpim@gmail.com>
2020-08-19 21:49:51 -0300

    Finalizado desafio 10

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -39,5 +39,68 @@ export const Container = styled.div`
         }
       }
     }
-  }
+
+    /* div.availability-container {
+      display: flex;
+      align-items: center;
+
+      p {
+        color: #3d3d4d;
+      }
+
+      .switch {
+        position: relative;
+        display: inline-block;
+        width: 88px;
+        height: 32px;
+        margin-left: 12px;
+
+        & input {
+          opacity: 0;
+          width: 0;
+          height: 0;
+        }
+
+        .slider {
+          position: absolute;
+          cursor: pointer;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background-color: #fff;
+          -webkit-transition: 0.4s;
+          transition: 0.4s;
+          border-radius: 16px;
+
+          &:before {
+            position: absolute;
+            content: '';
+            height: 20px;
+            width: 40px;
+            left: 8px;
+            bottom: 6px;
+            background-color: lightslategray;
+            -webkit-transition: 0.4s;
+            transition: 0.4s;
+            border-radius: 10px;
+          }
+        }
+
+        input:checked + .slider {
+          background-color: #3d3d4d;
+        }
+
+        input:focus + .slider {
+          box-shadow: 0 0 1px #2196f3;
+        }
+
+        input:checked + .slider:before {
+          -webkit-transform: translateX(32px);
+          -ms-transform: translateX(32px);
+          transform: translateX(32px);
+        }
+      }
+    }
+  } */
 `;
